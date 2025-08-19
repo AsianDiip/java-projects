6773cdeed1a5244e71965f8522ed5870bf8d1cae
James Chen <jamesc8453@gmail.com>
2025-08-19 17:15:16 -0400

    Finished simple http server that displays date and time

--- simple-http-server/SimpleHTTPServer.java
+++ simple-http-server/SimpleHTTPServer.java
@@ -1,8 +1,7 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDate;
 
 public class SimpleHTTPServer {
 
@@ -10,13 +9,10 @@ public class SimpleHTTPServer {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080");
         while(true){
-            Socket clientSocket = server.accept();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
-            while(!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
+            try (Socket socket = server.accept()) {
+                LocalDate today = LocalDate.now();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
         }
     }
